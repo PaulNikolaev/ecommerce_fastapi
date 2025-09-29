1c0faadddfe2e7e9a1744e61cd0d211191926c2a
Pavel Nikolaev <panikolaev7@gmail.com>
2025-09-29 12:03:54 +0300

    Changes SQLAlchemy models Category, Product

--- app/models/categories.py
+++ app/models/categories.py
@@ -1,7 +1,9 @@
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
+from sqlalchemy import ForeignKey, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
+from app.models.products import Product
 
 
 class Category(Base):
@@ -10,3 +12,7 @@ class Category(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    products: Mapped[list["Product"]] = relationship("Product", back_populates="category")
+
+
