2b1c7085f567002f67996973fdd684a9c84b7a63
Pavel Nikolaev <panikolaev7@gmail.com>
2025-09-30 10:53:34 +0300

    Initial alembic

--- app/models/categories.py
+++ app/models/categories.py
@@ -3,7 +3,6 @@ from sqlalchemy import ForeignKey, String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
-from app.models.products import Product
 
 
 class Category(Base):
--- app/models/products.py
+++ app/models/products.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import  Mapped, mapped_column, relationship
 
 
 from app.database import Base
-from app.models.categories import Category
 
 
 class Product(Base):
