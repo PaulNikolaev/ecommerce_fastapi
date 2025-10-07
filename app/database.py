30656b51c81ee18fc06bcb4efc070d0a01d47480
Pavel Nikolaev <panikolaev7@gmail.com>
2025-10-07 09:51:27 +0300

    Transition to postgresdb stage 2

--- app/database.py
+++ app/database.py
@@ -1,12 +1,35 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker
 
-
+# Строка подключения для SQLite
 DATABASE_URL = "sqlite:///ecommerce.db"
 
+# Создаём Engine
 engine = create_engine(DATABASE_URL, echo=True)
 
+# Настраиваем фабрику сеансов
 SessionLocal = sessionmaker(bind=engine)
+
+# --------------- Асинхронное подключение к PostgreSQL -------------------------
+
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv()
+
+# Строка подключения для PostgreSQl
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("Переменная окружения DATABASE_URL не установлена!")
+
+# Создаём Engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 
 
 class Base(DeclarativeBase):
--- app/migrations/env.py
+++ app/migrations/env.py
@@ -7,12 +7,26 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+import os
+from dotenv import load_dotenv
+
 from app.database import Base
 from app import models
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+ALEMBIC_DB_URL = os.environ.get("DATABASE_URL")
+
+if ALEMBIC_DB_URL:
+    # 3. Принудительно устанавливаем sqlalchemy.url в конфигурации Alembic
+    config.set_main_option('sqlalchemy.url', ALEMBIC_DB_URL)
+else:
+    # Опционально: выдаем ошибку, если переменная не найдена
+    print("ВНИМАНИЕ: Переменная DATABASE_URL не найдена. Alembic будет использовать URL из alembic.ini.")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -24,6 +38,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
