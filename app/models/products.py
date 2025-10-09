fafd7fafb53772da159749bd235ea78b810b3c88
Pavel Nikolaev <panikolaev7@gmail.com>
2025-10-09 09:05:30 +0300

    Added model User

--- app/models/products.py
+++ app/models/products.py
@@ -1,7 +1,6 @@
 from sqlalchemy import String, Boolean, Float, Integer, ForeignKey
 from sqlalchemy.orm import  Mapped, mapped_column, relationship
 
-
 from app.database import Base
 
 
@@ -16,5 +15,7 @@ class Product(Base):
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
+    seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
 
     category: Mapped["Category"] = relationship(back_populates="products")
+    seller = relationship("User", back_populates="products")
